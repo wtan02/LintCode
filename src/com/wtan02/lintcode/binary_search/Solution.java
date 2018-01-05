67b91455e8753d5aa9beb46b9fdb26e20f1c6d15
Normandy Tan <wtan02@outlook.com>
2018-01-04 23:27:10 -0800

    Find Peak Element

---
+++ src/com/wtan02/lintcode/binary_search/Solution.java
@@ -0,0 +1,30 @@
+package com.wtan02.lintcode.binary_search;
+
+class Solution {
+    /**
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    public int findPeak(int[] A) {
+        // write your code here
+        int start = 1, end = A.length-2; // 1.答案在之间，2.不会出界
+        while(start + 1 <  end) {
+            int mid = (start + end) / 2;
+            if (A[mid - 1] < A[mid] && A[mid] > A[mid + 1]) {
+                return mid;
+            }
+            else if(A[mid] < A[mid - 1]) {
+                end = mid;
+            }
+            else if(A[mid] < A[mid + 1]) {
+                start = mid;
+            }
+        }
+        if(A[start] < A[end]) {
+            return end;
+        } else {
+            return start;
+        }
+    }
+}
+
